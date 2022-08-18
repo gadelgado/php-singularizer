c6f3486dd5cbc69a47e4c7020b8585bb98e2c7cc
gadc1996 <gadc1996@gmail.com>
2022-08-18 16:22:15 -0600

    add singularize function

---
+++ src/Singularizer.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Gadelgado\Singularizer;
+
+class Singularizer
+{
+    const SINGULAR = [
+        '/(quiz)zes$/i' => '\\1',
+        '/(matr)ices$/i' => '\\1ix',
+        '/(vert|ind)ices$/i' => '\\1ex',
+        '/^(ox)en/i' => '\\1',
+        '/(alias|status)es$/i' => '\\1',
+        '/([octop|vir])i$/i' => '\\1us',
+        '/(cris|ax|test)es$/i' => '\\1is',
+        '/(shoe)s$/i' => '\\1',
+        '/(o)es$/i' => '\\1',
+        '/(bus)es$/i' => '\\1',
+        '/([m|l])ice$/i' => '\\1ouse',
+        '/(x|ch|ss|sh)es$/i' => '\\1',
+        '/(m)ovies$/i' => '\\1ovie',
+        '/(s)eries$/i' => '\\1eries',
+        '/([^aeiouy]|qu)ies$/i' => '\\1y',
+        '/([lr])ves$/i' => '\\1f',
+        '/(tive)s$/i' => '\\1',
+        '/(hive)s$/i' => '\\1',
+        '/([^f])ves$/i' => '\\1fe',
+        '/(^analy)ses$/i' => '\\1sis',
+        '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '\\1\\2sis',
+        '/([ti])a$/i' => '\\1um',
+        '/(n)ews$/i' => '\\1ews',
+        '/s$/i' => ''
+    ];
+
+    const IRREGULAR = [
+        'person' => 'people',
+        'man' => 'men',
+        'child' => 'children',
+        'sex' => 'sexes',
+        'move' => 'moves',
+    ];
+
+    const IGNORE = [
+        'equipment',
+        'information',
+        'rice',
+        'money',
+        'species',
+        'series',
+        'fish',
+        'sheep',
+        'press',
+        'sms',
+    ];
+
+    public static function singularize(string $word): string
+    {
+        $lower_word = strtolower($word);
+
+        if (in_array($lower_word, Self::IGNORE)) return $word;
+
+        if(array_key_exists($lower_word, Self::IRREGULAR)) return Self::IRREGULAR[$lower_word];
+
+        foreach (Self::SINGULAR as $rule => $replacement) {
+            if (preg_match($rule, $word)) {
+                return preg_replace($rule, $replacement, $word);
+            }
+        }
+    }
+}
